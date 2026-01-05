9e1716cd467e0fc3d8666f6166d0a7ee868932d9
danciu-valentin-nicolae <valentindanciu02@gmail.com>
2026-01-05 18:08:18 +0200

    removed app usage before built

--- RefConnect/Program.cs
+++ RefConnect/Program.cs
@@ -22,11 +22,7 @@ var builder = WebApplication.CreateBuilder(args);
 // - HTTPS uses the ASP.NET Core dev-certificate ("dotnet dev-certs https")
 // - Ports match `Properties/launchSettings.json`
 
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | 
-                       Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
-});
+
 
 
 
